62138847c0a05b91548a4bbda7d00bdc11477664
Colin Swan <colinswan.cs@gmail.com>
2023-05-01 20:58:42 +0100

    toast notification complete and overall logic coming together

--- src/hooks/dartboardHooks.js
+++ src/hooks/dartboardHooks.js
@@ -14,15 +14,55 @@ export const useDartboard = (playerCount, playerNames) => {
   const [player, setPlayer] = useState(1);
   const [darts, setDarts] = useState(3);
   const [roundScore, setRoundScore] = useState(0);
+  const [previousScores, setPreviousScores] = useState(
+    initialScores(playerCount)
+  );
 
   useEffect(() => {
     setPlayerScores(initialScores(playerCount));
   }, [playerCount]);
 
+  const nextPlayer = () => {
+    const newPlayer = player === playerCount ? 1 : player + 1;
+    setPlayer(newPlayer);
+    setDarts(3);
+    setRoundScore(0);
+  };
+
   const handleThrow = (points) => {
     if (darts === 0) return;
 
     const newScore = playerScores[player] - points;
+    const isDouble = points % 2 === 0 && points !== 50;
+
+    if (newScore < 0) {
+      toast.error("BUSTED!!");
+      setPlayerScores({ ...playerScores, [player]: previousScores[player] });
+      nextPlayer();
+      return;
+    }
+
+    if (newScore === 0 && isDouble) {
+      toast.success(`${playerNames[player] || `Player ${player}`} WINS!`);
+      setPlayerScores(initialScores(playerCount));
+      setPlayer(1);
+      setDarts(3);
+      setRoundScore(0);
+      return;
+    }
+
+    if (newScore === 1 || (newScore === 0 && !isDouble)) {
+      toast.error("BUSTED!!");
+      setPlayerScores({ ...playerScores, [player]: previousScores[player] });
+      nextPlayer();
+      return;
+    }
+
+    if (newScore > 1 && newScore <= 40 && newScore % 2 === 0) {
+      toast.info(`Finish on Double ${newScore / 2}`);
+    }
+
+    setPreviousScores({ ...previousScores, [player]: playerScores[player] });
     setPlayerScores({ ...playerScores, [player]: newScore });
 
     if (points === 50) {
@@ -38,13 +78,17 @@ export const useDartboard = (playerCount, playerNames) => {
     setRoundScore(newRoundScore);
 
     if (newDarts === 0) {
-      if (newRoundScore === 180) {
+      if (newRoundScore >= 100 && newRoundScore < 140) {
+        toast.success("TON-PLUS!");
+      } else if (newRoundScore === 140) {
+        toast.success("TON-FORTY!");
+      } else if (newRoundScore === 100) {
+        toast.success("TON!");
+      } else if (newRoundScore === 180) {
         toast.success("180!!");
       }
-      const nextPlayer = player === playerCount ? 1 : player + 1;
-      setPlayer(nextPlayer);
-      setDarts(3);
-      setRoundScore(0);
+
+      nextPlayer();
     }
   };
 
